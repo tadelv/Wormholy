ec9a3171dbaeb45c55041044b91445f6d14f7687
Vid Tadel <tadelv@gmail.com>
2021-04-02 07:58:31 +0200

    change target path in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Wormholy",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "WormholyTests",
-            dependencies: ["Wormholy"]),
+            dependencies: ["Wormholy"],
+            path: "Tests"),
     ]
 )
